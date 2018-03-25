7cd6f344579c0950140f6cd2dc845265cd2ad630
jason chen <jasonChen.cl1989@gmail.com>
2018-03-25 14:45:15 +0800

    repush

--- src/main.js
+++ src/main.js
@@ -6,6 +6,5 @@ const vdom = velement('div', { id: 'container' }, [
   velement('p', ['hello world']),
   velement('ul', [velement('li', ['item #1']), velement('li', ['item #2'])]),
 ]);
-
 const rootnode = vdom.render();
 document.body.appendChild(rootnode);  // eslint-disable-line
